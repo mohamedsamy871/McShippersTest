d5dfcf80cc85d27729a3a85018d01eebc7f37db9
mohamedsamy871 <mohamedsamy871@hotmail.com>
2021-12-04 23:53:41 +0200

    'nn'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './Authentication/login/login.component';
+import { ForgetPasswordComponent } from './Authentication/forget-password/forget-password.component';
+import { RegisterComponent } from './Authentication/register/register.component';
+import { ChangePasswordComponent } from './Authentication/change-password/change-password.component';
+import { ActivateEmailComponent } from './Authentication/activate-email/activate-email.component';
 const routes: Routes = [
   {
     component: HomePageComponent,
@@ -11,8 +15,22 @@ const routes: Routes = [
     component: LoginComponent,
     path:'login'
   },
-
-
+  {
+    component: ForgetPasswordComponent,
+    path:'forget'
+  },
+  {
+    component: RegisterComponent,
+    path:'register'
+  },
+  {
+    component: ChangePasswordComponent,
+    path:'change-password'
+  },
+  {
+    component: ActivateEmailComponent,
+    path:'activate-email'
+  },
 ];
 
 @NgModule({
